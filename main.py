1db0c526f73f2b6ba8b04f838edfe41ff94e649d
adam <adam.00228@hotmail.fr>
2023-05-05 12:08:09 +0200

    Pydantic

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 from fastapi import FastAPI
 from models import User, Gender,Role
 from typing import List
-from uuid import uuid4
+from uuid import UUID ,uuid4
 
 
+app= FastAPI()
 
-app = FastAPI()
+
 
 db: List[User] = [
-   User(
-         id=uuid4(),
-         first_name="Jamila",  
-         last_name="Ahmed",
-         gender=Gender.female,
-         roles=[Role.student]
+    User(
+         id=UUID("22ced1d5-5fda-4b17-9923-c50f40e0f0e6"),
+         first_name = "Jamila",  
+         last_name = "Ahmed",
+         gender = Gender.female,
+         roles = [Role.student]
+          ),
+    User(
+         id=UUID("d2437a9f-2f92-48b0-b089-75aad27e135a"),
+         first_name = "Alex",  
+         last_name = "Jones",
+         gender = Gender.male,
+         roles = [Role.admin, Role.user]
          ),
-   User(
-         id=uuid4(),
-         first_name="Alex",  
-         last_name="Jones",
-         gender=Gender.male,
-         roles=[Role.admin, Role.user])
 ]
 @app.get("/")
-async def root():
-   return{'HEllo': 'Mundo'}
+def root():
+    return{"Hello":"World"}       
 
-# uvicorn main:app --reload
 @app.get("/api/v1/users")
 async def fetch_users():
-   return db
+    return db,
+
+@app.post("/api/v1/users")
+async def register_user(user: User):
+    db.append(user)
+    return{"id":user.id}
 
 
--- models.py
+++ models.py
@@ -3,22 +3,19 @@ from pydantic import BaseModel
 from uuid import UUID, uuid4
 from enum import Enum
 
-
 class Gender (str,Enum):
-    male = "male"
-    female = "male"
+     male = "male"
+     female = "female"
 
 class Role (str, Enum):
-    admin = "admin"
-    user ="User"
-    student = "student"
+     admin = "admin"
+     user ="User"
+     student = "student"
 
 class User(BaseModel):
-    id: Optional[UUID] = uuid4()
-    first_name: str
-    last_name : str
-    middle_name : Optional[str]
-    gender : Gender
-    role : List[Role]
-
-
+     id: Optional[UUID] = uuid4()
+     first_name: str
+     last_name : str
+     middle_name : Optional[str]
+     gender : Gender
+     roles : List[Role]
